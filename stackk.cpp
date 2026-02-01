21a4645e4803243337a58ca6826eef92fe4d6fd1
Udisha singh <udishasingh865@gmail.com>
2026-02-02 02:04:34 +0530

    next greater element 2

--- stackk.cpp
+++ stackk.cpp
@@ -1,3 +1,4 @@
+/* VALID PARANTHESIS */
 class Solution {
 public:
     bool isValid(string s) {
@@ -16,5 +17,25 @@ public:
             }
         }
         return st.empty();
+    }
+};
+
+/*NEXT GREATER ELEMENT 2*/
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> nge(n , -1);
+        stack<int> st;
+        for(int i = (2*n)-1 ; i >= 0; i--){
+            while(!st.empty() && nums[i % n] >= st.top()){
+                st.pop();
+            }
+            if(i < n){
+                nge[i] = st.empty() ? -1 : st.top();
+            }
+            st.push(nums[i % n]);
+        }
+        return nge;
     }
 };
