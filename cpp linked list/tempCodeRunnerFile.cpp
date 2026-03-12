49468485629940b5b99436b8b4bc540317807311
Udisha singh <udishasingh865@gmail.com>
2026-03-12 18:55:45 +0530

    Removed mistakenly added folders

--- cpp linked list/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-//PRINTING THE DOUBLY LINKED LIST 
-void print(Node* head){
-    Node* temp = head;
-    while(temp){
-        cout << temp->data << " ";
-        temp = temp->next;
-    }
-    cout << endl;
-}
